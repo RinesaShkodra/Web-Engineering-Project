14a770e6031c97b59aa49b314c1e896e610be7b3
RinesaShkodra <rs51168@ubt-uni.net>
2022-01-21 13:38:09 +0100

    did a few changes on the page content, started creating the admin dashboard

---
+++ admin/index.php
@@ -0,0 +1,50 @@
+<html>
+<head> <title> Taste Of Rome </title>
+
+<link href="../CSS/admin.css" rel="stylesheet">
+</head>
+   <body>
+       
+       <div class="menu text-center">
+          <div class ="wrapper">
+               <ul>
+                  <li><a href ="#">Home </a> </li>
+                 <li><a href ="#">Admin </a> </li>
+                 <li><a href ="#">Category </a> </li>
+                 <li><a href ="#">Food </a> </li>
+                 <li><a href ="#">Order</a> </li>
+              </ul>
+           </div>
+      </div>
+
+      <div class ="main-content">
+          <div class ="wrapper">
+              <h1>DASHBOARD </h1>
+              <br> <br>
+
+              <div class="col-4">
+                  <h1>5</h1>
+                  <br>
+                  Categories
+             </div>
+
+             <div class="col-4">
+                  <h1>5</h1>
+                  <br>
+                  Categories
+             </div>
+             <div class="col-4">
+                  <h1>5</h1>
+                  <br>
+                  Categories
+             </div>
+             <div class="col-4">
+                  <h1>5</h1>
+                  <br>
+                  Categories
+              </div>
+             </div>
+        </div>
+        <div class="fix"></div>
+</body>
+    </html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,25 @@
+<html>
+<head> <title> Taste Of Rome </title>
+
+<link href="../CSS/admin.css" rel="stylesheet">
+</head>
+   <body>
+       
+       <div class="menu text-center">
+          <div class ="wrapper">
+               <ul>
+                  <li><a href ="#">Home </a> </li>
+                 <li><a href ="#">Admin </a> </li>
+                 <li><a href ="#">Category </a> </li>
+                 <li><a href ="#">Food </a> </li>
+                 <li><a href ="#">Order</a> </li>
+              </ul>
+           </div>
+      </div>
+
+      <div class ="main-content">
+          <div class ="wrapper">
+              <h1>manage admin </h1>
+        <div class="fix"></div>
+</body>
+    </html>
